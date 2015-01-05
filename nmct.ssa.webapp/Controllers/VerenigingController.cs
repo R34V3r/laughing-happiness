22933774f6d35adefe2f09d121a1cb7a8fb45acc
R34V3r <Matthias.Van.Onsem@outlook.be>
2015-01-05 20:39:55 +0100

    comeon3

---
+++ nmct.ssa.webapp/Controllers/VerenigingController.cs
@@ -0,0 +1,73 @@
+﻿using nmct.ba.cashlessproject.model.Web;
+using nmct.ssa.webapp.DataAcces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nmct.ssa.webapp.Controllers
+{
+    [Authorize]
+    public class VerenigingController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Vereniging> verenigingen = new List<Vereniging>();
+            verenigingen = VerenigingDA.GetVerenigingen();
+            return View(verenigingen);
+        }
+
+        [HttpGet]
+        public ActionResult Nieuw()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nieuw(Vereniging vereniging)
+        {
+            int rowsaffected = VerenigingDA.NieuweVereniging(vereniging);
+            if (rowsaffected == 0)
+                return View("Error");
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Bewerken(int? id)
+        {
+            if (!id.HasValue)
+                return RedirectToAction("Index");
+
+            Vereniging vereniging = new Vereniging();
+            vereniging = VerenigingDA.GetVerenigingById(id.Value);
+
+            return View(vereniging);
+        }
+
+        [HttpPost]
+        public ActionResult Bewerken(Vereniging vereniging)
+        {
+            int rowsaffected = VerenigingDA.UpdateVereniging(vereniging);
+            if (rowsaffected == 0)
+                return View("Error");
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+                return RedirectToAction("Index");
+
+            Vereniging vereniging = new Vereniging();
+            vereniging = VerenigingDA.GetVerenigingById(id.Value);
+
+            return View(vereniging);
+        }
+
+    }
+}
--- nmct.ssa.webapp/Migrations/Configuration.cs
+++ nmct.ssa.webapp/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace nmct.ssa.webapp.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+    using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using nmct.ssa.webapp.Models;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<nmct.ssa.webapp.Models.ApplicationDbContext>
     {
@@ -12,23 +15,40 @@ namespace nmct.ssa.webapp.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        
+
         protected override void Seed(nmct.ssa.webapp.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            string roleAdmin = "Administrator";
+            string roleNormalUser = "User";
+            IdentityResult roleResult;
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
+            if (!RoleManager.RoleExists(roleNormalUser))
+            {
+                roleResult = RoleManager.Create(new IdentityRole(roleNormalUser));
+            }
 
+            if (!RoleManager.RoleExists(roleAdmin))
+            {
+                roleResult = RoleManager.Create(new IdentityRole(roleAdmin));
+            }
 
+            if (!context.Users.Any(u => u.Email.Equals("matthias.van.onsem@outlook.be")))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser()
+                {
+                    Email = "matthias.van.onsem@outlook.be",
+                    UserName = "matthias.van.onsem@outlook.be"
+                };
+                manager.Create(user, "-Password1");
+                manager.AddToRole(user.Id, roleAdmin);
+            }
         }
+
+    
     }
 }
